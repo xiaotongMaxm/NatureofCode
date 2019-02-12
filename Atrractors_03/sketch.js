db1b8ea51ed9ee5721e6156a9a07886376fd3788
xiaotongMaxm <42880672+xiaotongMaxm@users.noreply.github.com>
2019-02-11 22:29:05 -0500

    Add files via upload

---
+++ Atrractors_03/sketch.js
@@ -0,0 +1,25 @@
+let attractor;
+let particles = [];
+
+function setup() {
+  createCanvas(640, 640);
+  background(255);
+  for (let i = 0; i < 100; i++) {
+    particles.push(new Particle(320, 160));
+  }
+  attractor = createVector(320, 320);
+
+}
+
+function draw() {
+  stroke(255);
+  strokeWeight(1);
+  point(attractor.x, attractor.y);
+
+  for (let i = 0; i < particles.length; i++) {
+    let particle = particles[i];
+    particle.attracted(attractor); //any given particle can be attracted by attractor
+    particle.update();
+    particle.show();
+  }
+}
